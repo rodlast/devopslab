26ad1536622083f85334ba7606a0754ef95aa30b
rodlast <62860580+rodlast@users.noreply.github.com>
2022-01-26 16:10:07 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -8,16 +8,6 @@ csrf = CSRFProtect(app)
 @app.route("/")
 def pagina_inicial():
     return "Hello World Rodrigo Fior"
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
+    
 if __name__ == '__main__':
     app.run()
